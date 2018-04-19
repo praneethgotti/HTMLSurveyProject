cdd5161755667a315087fb65e9e5ddd6f335b8d8
praneeth694 <praneeth694@gmail.com>
2018-04-19 10:41:02 -0400

    First committ

---
+++ project1/project1/frmStudent.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmStudent.aspx.cs" Inherits="project1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            margin-top: 43px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="z-index: 1; left: 1px; top: 371px; position: absolute; height: 27px; width: 963px">
+            <asp:Label ID="lblStudentID" runat="server" Text="Label"></asp:Label>
+        </div>
+        <asp:Label ID="lblName" runat="server" Text="Label" style="z-index: 1; left: 10px; top: 77px; position: absolute"></asp:Label>
+        <p>
+        <asp:Label ID="lblTeacherScore" runat="server" Text="Label" style="z-index: 1; left: 6px; top: 155px; position: absolute; margin-top: 0px"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="lblCourseScore" runat="server" Text="Label" style="z-index: 1; left: 7px; top: 192px; position: absolute"></asp:Label>
+        </p>
+        <asp:Label ID="lblProfessorGrade" runat="server" Text="Label" style="z-index: 1; left: 8px; top: 250px; position: absolute; bottom: 281px;"></asp:Label>
+        <p style="z-index: 1; left: 4px; top: 281px; position: absolute; height: 19px; width: 354px">
+            <asp:Label ID="lblCourseGrade" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ project1/project1/frmStudent.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace project1
+{
+
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        string teacherGrade;
+        string courseGrade;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                lblName.Text = " Thank You " + Request["Name"].ToString() + "  for filling out the survey";
+                lblStudentID.Text = "Your TUID is : " + Request["TUID"].ToString();
+
+            
+                // Teacher Calculations
+                double teacher1 = Convert.ToDouble(Request["Teacher1"]);
+                double teacher2 = Convert.ToDouble(Request["Teacher2"]);
+                double teacher3 = Convert.ToDouble(Request["Teacher3"]);
+                double teacher4 = Convert.ToDouble(Request["Teacher4"]);
+                double teacher5 = Convert.ToDouble(Request["Teacher1"]);
+                double teacher6 = Convert.ToDouble(Request["Teacher6"]);
+                double teacher7 = Convert.ToDouble(Request["Teacher7"]);
+                double teacher8 = Convert.ToDouble(Request["Teacher8"]);
+
+
+                //Course Calculations
+                double course1 = Convert.ToDouble(Request["Course1"]);
+                double course2 = Convert.ToDouble(Request["Course2"]);
+                double course3 = Convert.ToDouble(Request["Course3"]);
+                double course4 = Convert.ToDouble(Request["Course4"]);
+                double course5 = Convert.ToDouble(Request["Course5"]);
+                double course6 = Convert.ToDouble(Request["Course6"]);
+                double course7 = Convert.ToDouble(Request["Course7"]);
+                double course8 = Convert.ToDouble(Request["Course8"]);
+                double course9 = Convert.ToDouble(Request["Course9"]);
+                double course10 = Convert.ToDouble(Request["Course10"]);
+                double course11 = Convert.ToDouble(Request["Course11"]);
+                double course12 = Convert.ToDouble(Request["Course12"]);
+
+
+                double teacherScore = teacher1 + teacher2 + teacher3 + teacher4 +
+                teacher5 + teacher6 + teacher7 + teacher7 + teacher8;
+
+                double courseScore = course1 + course2 + course3 + course4 +
+                    course5 + course6 + course7 + course8 + course9 + course10 +
+                    course11 + course12;
+
+
+                lblTeacherScore.Text = "The score you gave your teacher is a " + teacherScore;
+                lblCourseScore.Text = "The score you gave your course is a " + courseScore;
+
+
+                if (teacherScore >= 36)
+                {
+                    teacherGrade = "A";
+                }
+
+                if (teacherScore >= 32)
+                {
+                    teacherGrade = "B";
+                }
+
+                if (teacherScore >= 28)
+                {
+                    teacherGrade = "C";
+                }
+
+                if (teacherScore >= 24)
+                {
+                    teacherGrade = "D";
+                }
+
+                if (teacherScore < 24)
+                {
+                    teacherGrade = "F";
+                }
+
+                if (courseScore >= 54)
+                {
+                    courseGrade = "A";
+                }
+
+                if (courseScore >= 48)
+                {
+                    courseGrade = "B";
+                }
+
+                if (courseScore >= 42)
+                {
+                    courseGrade = "C";
+                }
+
+                if (courseScore >= 36)
+                {
+                    courseGrade = "D";
+                }
+
+                if (courseScore < 36)
+                {
+                    courseGrade = "F";
+                }
+
+                lblProfessorGrade.Text = "The grade you gave your teacher is a " + teacherGrade.ToString();
+                lblCourseGrade.Text = "The grade you gave your course is a " + courseGrade.ToString();
+
+               
+                
+            }
+        }
+
+            
+        
+        }
+    }
